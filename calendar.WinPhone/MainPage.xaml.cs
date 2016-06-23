57a41a592e99e25ad2df89546725043708adabea
Paul Johnson <paul@all-the-johnsons.co.uk>
2016-06-23 17:35:16 +0100

    initla import of winphone code

---
+++ calendar.WinPhone/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using Windows.UI.Xaml;
+using Xamarin.Forms;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=391641
+
+namespace calendar.WinPhone
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+
+            var rawpixelperview = Windows.Graphics.Display.DisplayInformation.GetForCurrentView().RawPixelsPerViewPixel;
+            calendar.App.ScreenSize = new Xamarin.Forms.Size(Window.Current.Bounds.Width * rawpixelperview, Window.Current.Bounds.Height * rawpixelperview);
+            LoadApplication(new calendar.App());
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.
+        /// This parameter is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            // TODO: Prepare page for display here.
+
+            // TODO: If your application contains multiple pages, ensure that you are
+            // handling the hardware Back button by registering for the
+            // Windows.Phone.UI.Input.HardwareButtons.BackPressed event.
+            // If you are using the NavigationHelper provided by some templates,
+            // this event is handled for you.
+        }
+    }
+}
